832c8fd45fa5b4a006b7f71fbbf34ddc4f8a02f7
Artrell Coker <trellson13@gmail.com>
2022-12-20 18:29:59 -0500

    Got api url and  proper imports set up

--- main.py
+++ main.py
@@ -1,6 +1,25 @@
+import urllib.parse
+import requests
+
+
+api_url = https://www.googleapis.com/books/v1/voulumes?q={userinput}
+
+
+response = requests.get(url).json()
+userSearch = 
+
+
+print(response.status)
+
+
 def main():
 
-    print('Artrell Is a Python Developer')
+   userList = []
+
+   print ("***PY Book Search***")
+   print ("What would you like to search?")
+
+
 
 if __name__ == '__main__':
     main()
