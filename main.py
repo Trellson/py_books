7b1ef2930c457ff251c288eb38ea7abee05ea562
Trellson <97971270+Trellson@users.noreply.github.com>
2022-12-21 10:05:08 -0500

    Revert "User interface"

--- main.py
+++ main.py
@@ -1,55 +1,6 @@
-import requests
-import urllib
-
-response = requests.get("http://www.google.com")
-
-
 def main():
-   try:
-      # creating book list 
-      readingList = []
-      infile = open('userBooks.txt', 'r')
-      line = infile.readline()
-      while line:
-         readingList.append(line.rstrip('\n').split(','))
-         line = infile.readline()
-      infile.close()
 
-   except FileNotFoundError :
-      print('the <userBooks.txt> file is not found!')
-      print('initializing <userBooks.txt>...')
-      readingList = []
+    print('Artrell Is a Python Developer')
 
-
-   #menu options for user to pick
-   choice = 0
-   while choice !=3:
-      print ("***PY Book Search***")
-      print('1) Search a book')
-      print('2) Display Reading List')
-      print('3) Quit')
-      choice = int(input())
-     
-      if choice == 1: 
-         userSearch = input ('What would you like to search?')
-         #readingList.append selected book 
-         print('Loading seletions... Please wait')
-      
-      elif choice == 2:
-         print('Displaying Reading List \n =====================')
-         for i in range(len(readingList)):
-            print(readingList)
-
-      elif choice == 3:
-         print('Terminating program')
-
-   print('Come Back Soon!')
-
-   outfile = open('userBooks.txt', 'w')
-   for book in readingList:
-      outfile.write(','.join(book) + '\n')
-   outfile.close()
-
-   
 if __name__ == '__main__':
-   main()
+    main()
