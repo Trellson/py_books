ebb67779e0752254cc085dfb664a92996fe0cbee
Artrell Coker <trellson13@gmail.com>
2022-12-21 03:33:49 -0500

    retreived book author and title from api

--- main.py
+++ main.py
@@ -4,18 +4,20 @@ import urllib
 
 def main():
 
-   userSearch = 'pen'
+   userSearch = 'more than a carpenter'
    api_url = 'https://www.googleapis.com/books/v1/volumes?q='+userSearch+'&maxResults=5'
-
-
-
-
    response = requests.get(api_url).json()
+   items = response['items']
+  
+   for i in range(0,len(response)+1):
+      title = items[i]['volumeInfo']['title']
+      author = items[i]['volumeInfo']['authors']
+     # publisher = items[i]['volumeInfo']['publisher']  For some reason the publisher would not work...
 
-   print(response)
-
-   print (api_url)
-
+   
+   print(title)
+   print(author)
+   #print(publisher)
 
    try:
       # creating book list 
@@ -46,6 +48,7 @@ def main():
          userSearch = input ('What would you like to search?')
          #readingList.append selected book 
          print('Loading seletions... Please wait')
+         print(response)
       
       elif choice == 2:
          print('Displaying Reading List \n =====================')
