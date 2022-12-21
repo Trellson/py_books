bb62b9beb49b629a254b4ee59c9cc75389a8b889
Artrell Coker <trellson13@gmail.com>
2022-12-20 23:33:58 -0500

    created cml and logic for how it flows

--- main.py
+++ main.py
@@ -1,25 +1,36 @@
-import urllib.parse
 import requests
+import urllib
 
-
-api_url = https://www.googleapis.com/books/v1/voulumes?q={userinput}
-
-
-response = requests.get(url).json()
-userSearch = 
-
-
-print(response.status)
+response = requests.get("http://www.google.com")
 
 
 def main():
 
-   userList = []
 
-   print ("***PY Book Search***")
-   print ("What would you like to search?")
+   #menu options for user to pick
+   choice = 0
+   while choice !=3:
+      print ("***PY Book Search***")
+      print('1) Search a book')
+      print('2) Display Reading List')
+      print('3) Quit')
+      choice = int(input())
+     
+      if choice == 1: 
+         userSearch = input ('What would you like to search?')
+         #readingList.append selected book 
+         print('Loading seletions... Please wait')
+      
+      elif choice == 2:
+         print('Displaying Reading List \n =====================')
+         for i in range(len(readingList)):
+            print(readingList)
 
+      elif choice == 3:
+         print('Terminating program')
 
+   print('Come Back Soon!')
 
+   
 if __name__ == '__main__':
-    main()
+   main()
